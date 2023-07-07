3a84028dcd2a6ec8708ac40786241eccab095a1c
Michael Steinmoetzger <m.steinmoetzger@gmail.com>
2023-07-07 23:15:23 +0200

    working on gravity change

--- Assets/Player/CharacterControllerNew.cs
+++ Assets/Player/CharacterControllerNew.cs
@@ -1,14 +1,37 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterControllerNew : MonoBehaviour {
 
     private InputActionManager _inputActionManager;
+    private Vector2 _velocity;
+    private Rigidbody2D _rigidbody;
+
+    
     
     private void Awake() {
         this._inputActionManager = new InputActionManager();
+        this._velocity = new Vector2(0, 0);
+        this._rigidbody = GetComponent<Rigidbody2D>();
+
+        this._inputActionManager.Player.ChangeGravity.performed += OnChangeGravity;
+    }
+
+    private void OnChangeGravity(InputAction.CallbackContext callbackContext) {
+
+        float targetGravityScale = _rigidbody.gravityScale * -1;
+        const float tolerance = 0.01f;
+        
+        while (Math.Abs(_rigidbody.gravityScale - targetGravityScale) > tolerance) {
+            if (targetGravityScale < 1) {
+                _rigidbody.gravityScale--;
+                continue;
+            }
+
+            _rigidbody.gravityScale++;
+        }
+
     }
 
     private void OnEnable() {
@@ -18,16 +41,11 @@ public class CharacterControllerNew : MonoBehaviour {
     private void OnDisable() {
         this._inputActionManager.Disable();
     }
+    
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private void FixedUpdate() {
+        _rigidbody.velocity = _velocity;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
