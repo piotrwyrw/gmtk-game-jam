419b49405c865688ac170e44d0368485b98c7973
Aturym <rastipatak@hotmail.com>
2023-07-07 21:16:52 +0200

    init new branch

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //not main branch
     }
 
     // Update is called once per frame
