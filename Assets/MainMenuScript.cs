4f7f7e96c5b3f5737b915aa12dbd650adacbb003
Semmelon <sameliassamy@gmail.com>
2023-07-08 12:37:08 +0200

    Menu - Version 4

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuScript : MonoBehaviour
 {
 
     private int index = 1;
+
+    public TextMeshProUGUI textMeshProUGUI;
+
     public void PressPlay() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + index);
     }
@@ -17,13 +22,16 @@ public class MainMenuScript : MonoBehaviour
 
     public void setIndexLevel1() { 
         index = 1;
+        textMeshProUGUI.text = "Level 1";
     }
 
     public void setIndexLevel3() { 
         index = 3;
+        textMeshProUGUI.text = "Level 3";
     }
 
     public void setIndexLevel2() { 
         index = 2;
+        textMeshProUGUI.text = "Level 2";
     }
 }
