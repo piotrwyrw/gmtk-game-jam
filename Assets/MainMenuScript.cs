6f27de4e914e0e3d50c3420ad304b9a2bc5014b8
Semmelon <sameliassamy@gmail.com>
2023-07-08 23:11:51 +0200

    Menu mit 3 Level (space für 8 level)

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -36,8 +36,37 @@ public class MainMenuScript : MonoBehaviour
         textMeshProUGUI.text = "Level 2";
     }
 
-    public void setInfinityMode() {
+    public void serIndexLevel4() {
         index = 4;
+        textMeshProUGUI.text = "Level 4";
+    }
+
+    public void serIndexLevel5()
+    {
+        index = 5;
+        textMeshProUGUI.text = "Level 5";
+    }
+
+    public void serIndexLevel6()
+    {
+        index = 6;
+        textMeshProUGUI.text = "Level 6";
+    }
+
+    public void serIndexLevel7()
+    {
+        index = 7;
+        textMeshProUGUI.text = "Level 7";
+    }
+
+    public void serIndexLevel8()
+    {
+        index = 8;
+        textMeshProUGUI.text = "Level 8";
+    }
+
+    public void setInfinityMode() {
+        index = 9;
         textMeshProUGUI.text = "Infinity Mode";
     }
 }
